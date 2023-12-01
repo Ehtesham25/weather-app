78a9b885dd703993b93e31e23c121283c23111f5
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-11-30 16:21:28 -0800

    build new app

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+import  React from 'react';
+import { Appbar,Title } from 'react-native-paper';
+
+const Header = (props) => {
+ 
+  return (
+    <Appbar.Header
+    theme={{
+        colors:{
+            primary:"#00aaff",
+        }
+    }}
+    >
+    <Appbar.Content style={{flexDirection:'row', colors:"white" ,justifyContent:"center"}} title={props.name} />
+ 
+  </Appbar.Header>
+  );
+}
+
+export default Header;
